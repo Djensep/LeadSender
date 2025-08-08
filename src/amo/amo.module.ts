e9212a18f529fceb065d0b3a60e991710067d294
Lee Djen Sep <leedjensep@gmail.com>
2025-08-09 00:43:42 +0500

    ADD typeORM imports

--- src/amo/amo.module.ts
+++ src/amo/amo.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AmoService } from './amo.service';
 import { AmoController } from './amo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AmoEntity } from './amo.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AmoEntity])],
   controllers: [AmoController],
   providers: [AmoService],
 })
--- src/deals/deals.module.ts
+++ src/deals/deals.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DealsService } from './deals.service';
 import { DealsController } from './deals.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DealsEntity } from './entity/deals.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DealsEntity])],
   controllers: [DealsController],
   providers: [DealsService],
 })
