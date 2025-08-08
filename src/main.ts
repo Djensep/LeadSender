3d7fdab101c088eb40ee6e0098dc6d07ab685729
Lee Djen Sep <leedjensep@gmail.com>
2025-08-09 00:06:55 +0500

    ADD swagger, validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder().setTitle('amoCRM').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 8000);
+
+  console.log(`Server: http://localhost:${process.env.PORT ?? 8000}`);
+  console.log(`Swagger: http://localhost:${process.env.PORT ?? 8000}/docs`);
 }
 bootstrap();
